3c10dde1888ed9978589e10a5511234e5a2ef7a7
Sandeep Chandupatla <chandupatlasandeep1990@gmail.com>
2025-12-02 12:50:15 -0500

    2723 Add two promises problem solved

---
+++ 2723-Add-Two-Promises/two-promises.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    let value1= await promise1;
+    let value2 =await promise2;
+    return value1+value2
+};
